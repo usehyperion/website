76249ee65f4442eded33080d55e89c2ff52ce1e0
Oliver Rose <hello@olivermrose.com>
2025-12-04 19:09:06 -0500

    feat: add `mdsx`

---
+++ mdsx.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'mdsx';
+
+export default defineConfig({
+	extensions: ['.md']
+});
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsx } from 'mdsx';
+import mdsxConfig from './mdsx.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [mdsx(mdsxConfig), vitePreprocess()],
+	extensions: ['.svelte', '.md'],
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
