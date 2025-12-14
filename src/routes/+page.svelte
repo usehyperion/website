17b3cb351ef8353a7807c4e7ef9346760e2ff8e9
Oliver Rose <hello@olivermrose.com>
2025-12-13 21:43:59 -0500

    feat: add product image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,14 @@
 	import Download from '$lib/components/Download.svelte';
 	import Features from '$lib/components/Features.svelte';
 	import Hero from '$lib/components/Hero.svelte';
+	import preview from '$lib/assets/preview.png';
 </script>
 
 <Hero />
+
+<div class="mx-auto max-w-6xl pt-10 pb-2">
+	<enhanced:img src={preview} alt="Preview of the application" />
+</div>
 
 <Features />
 <Download />
