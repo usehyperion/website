4e77be6ed5fde436fb03c11578de85b4ba05d0a9
Oliver Rose <hello@olivermrose.com>
2025-12-19 15:52:37 -0500

    feat: add cta to hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import DownloadSimple from 'phosphor-svelte/lib/DownloadSimple';
 	import glowBottom from '$lib/assets/glow-bottom.svg';
 	import Particles from '$lib/components/Particles.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import CallToAction from './CallToAction.svelte';
 </script>
 
 <section id="hero" class="relative mx-auto max-w-6xl px-4 sm:px-6">
@@ -18,8 +20,12 @@
 
 	<div class="pt-32 pb-16 md:pt-52 md:pb-32">
 		<div class="mx-auto max-w-4xl text-center">
+			<CallToAction href="https://github.com/usehyperion/hyperion/releases">
+				Hyperion is now in beta
+			</CallToAction>
+
 			<h1
-				class="from-foreground/50 via-foreground to-foreground/50 bg-linear-to-r bg-clip-text pb-4 text-transparent"
+				class="from-foreground/50 via-foreground to-foreground/50 bg-linear-to-r bg-clip-text py-4 text-transparent"
 			>
 				Chat Smarter with Hyperion
 			</h1>
@@ -32,7 +38,11 @@
 			<div
 				class="mx-auto max-w-xs space-y-4 sm:inline-flex sm:max-w-none sm:justify-center sm:space-y-0 sm:space-x-4"
 			>
-				<Button href="https://github.com/usehyperion/hyperion/releases">Download</Button>
+				<Button href="https://github.com/usehyperion/hyperion/releases">
+					<DownloadSimple />
+					Download
+				</Button>
+
 				<Button href="#features" variant="secondary">Explore features</Button>
 			</div>
 		</div>
